f55147adaffbdad0af1761ea4c7f1aeea80c8141
Tao Afpa <taoafpa@gmail.com>
2021-01-12 11:47:03 +0100

    Change Personne

--- src/fr/afpa/cda/cours/heritage/Personne.java
+++ src/fr/afpa/cda/cours/heritage/Personne.java
@@ -1,7 +1,21 @@
 package fr.afpa.cda.cours.heritage;
 
+import java.util.Date;
+
 public class Personne {
+	
+	/*
+	 * Personnes  : 
+		Attributs : nom , prenom , dateDeNaissance, sexe
+		Méthodes : communiquer() , travailler() , ecrire()
+
+	 * */
+	
+	
 	private String nom ;
+	private String prenom ;
+	private Date dateDeNaissance;
+	private char sexe ;
 	private int cin ;
 	
 	public Personne(String nom, int cin) {
@@ -16,6 +30,20 @@ public class Personne {
 	public final void lire() {
 		System.out.println("Je suis un humain qui sait lire !!");
 	}
+	
+	
+	public  void communiquer() {
+		System.out.println("Je suis un humain qui communique !!");
+	}; 	
+	public  void travailler() {
+		System.out.println("Je suis un humain qui travaille !!");
+	}; 
+	public  void ecrire() {
+		System.out.println("Je suis un humain qui ecrire !!");
+	} ;
+	
+	
+	
 	
 	@Override
 	public String toString() {
